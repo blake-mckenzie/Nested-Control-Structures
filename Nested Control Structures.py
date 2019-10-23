57d9f5b799b7a3a0390feb5d4e4d9e16e77376b1
Blake McKenzie <02blake.mckenzie@gulllakecs.org>
2019-10-23 13:46:28 -0400

    Initial Commit for the For Loop + While Loop Program

--- Nested Control Structures.py
+++ Nested Control Structures.py
@@ -14,3 +14,17 @@ for i in range (3):
     print ("Outer For Loop: " + str(i))
     for l in range (2):
         print ("     Inner For Loop: " + str(l))
+
+
+
+print ("\n****************\n")
+
+
+'''
+Programmer: Blake McKenzie
+Date: 10.23.19
+Program: Categories
+This program will ask users for an interest of them
+then ask for two items related to that interest
+'''
+
